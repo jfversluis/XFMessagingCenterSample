f2d7cd320d73c71e957bf117c70b15dbc658aad3
Gerald Versluis <gerald@verslu.is>
2021-01-31 20:52:22 +0100

    Add MessagingCenter Sample

--- XFMessagingCenterSample/MainPage.xaml.cs
+++ XFMessagingCenterSample/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,28 @@ namespace XFMessagingCenterSample
 {
     public partial class MainPage : ContentPage
     {
+        public ObservableCollection<string> MessagingItems { get; set; } = new ObservableCollection<string>();
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        // Subscribe (also to my YouTube Channel ;-))
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            MessagingCenter.Subscribe<Page, DateTime>(this, "tick", (p, datetime) =>
+            {
+                MessagingItems.Add($"Message received at {datetime}");
+            });
+        }
+
+        // Go to page
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SenderPage());
         }
     }
 }
---
+++ XFMessagingCenterSample/SenderPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFMessagingCenterSample
+{
+    public partial class SenderPage : ContentPage
+    {
+        public SenderPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            MessagingCenter.Send<Page, DateTime>(this, "tick", DateTime.Now);
+        }
+
+        async void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
